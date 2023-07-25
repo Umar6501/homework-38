945e736cc746d8afd803dcd0dd094a1481f57e34
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-07-25 14:14:45 +0500

    homework-38

---
+++ main.js
@@ -0,0 +1,66 @@
+// 1 misol
+// let B = 10;
+// let A = 12;
+// let Y = -(B / 2) * (A ** 2 + B ** 2);
+// console.log(Y);
+
+// 2 misol
+// let B = 2;
+// let A = 2;
+// let C = 2;
+// let D = 2;
+// let Y = (A + B) / (C + D) + B ** 2;
+// console.log(Y);
+
+// 3 misol
+// let A = 2;
+// let D = 2;
+// let C = 2;
+// let Y = 2 * (A ** D + (4 * C) ** 2 / 3);
+// console.log(Y);
+
+// 4 misol
+// let A = 1;
+// let B = 1;
+// let C = 1;
+// let D = 1;
+// let Y = (1 / A ** 2) * (B / 10) ** 3 * (C + D) ** 2;
+// console.log(Y);
+
+// 5 misol
+// let A = 4;
+// let B = 4;
+// let C = 4;
+// let D = 4;
+// let Y = (A / B - 1) / (C / (D - 1)) ** 2;
+// console.log(Y);
+
+// 6 misol
+// let A = 1;
+// let B = 1;
+// let C = 1;
+// let D = 1;
+// let Y = (A + B) / (C + (D / A + C));
+// console.log(Y);
+
+// 7 misol
+// let A = 1;
+// let B = 1;
+// let C = 1;
+// let Y = ((A + B + C) ** 2 - (A - B - C) ** 2) ** 2;
+// console.log(Y);
+
+// 8 misol
+// let A = 1;
+// let B = 1;
+// let C = 1;
+// let Y = (A ** 2 + B ** 2 + C ** 3) / (A * B * C);
+// console.log(Y);
+
+// 9 misol
+// let A = 1;
+// let B = 1;
+// let C = 1;
+// let D = 1;
+// let Y = B * (3 ** D / 3) + B * (2 ** C / 3);
+// console.log(Y);
